3a1e36c69d6d249642da54a35896430e7de7ed37
hension <myhension@gmail.com>
2026-03-24 17:59:28 +0800

    chore: bump version to 2.6.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hension.havenx"
         minSdk = 26
         targetSdk = 35
-        versionCode = 81
-        versionName = "2.6.4"
+        versionCode = 82
+        versionName = "2.6.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
