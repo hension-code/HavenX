0c3d01957d10f332322b57339b19a077170d3414
hension <myhension@gmail.com>
2026-03-24 18:17:54 +0800

    fix: update APK matching pattern in release.yml & bump to 2.6.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hension.havenx"
         minSdk = 26
         targetSdk = 35
-        versionCode = 82
-        versionName = "2.6.5"
+        versionCode = 83
+        versionName = "2.6.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
